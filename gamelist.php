0d0c5ae7a162d911e3e4edfb180e7f6c4e95201a
JTEyolf <JTEyolf@users.noreply.github.com>
2016-11-13 20:57:44 -0500

    Added some commented out mysql connection

--- gamelist.php
+++ gamelist.php
@@ -5,7 +5,34 @@
     <link rel="stylesheet" type="text/css" href="styles.css">
 </head>
 <body>
-    <h1>Videogames of 2016</h1>
+    <h1>Videogames of 2016</h1><hr >
     
+    <table align="left" cellspacing="5" cellpadding="8">
+        <tr><td align="left"><b>ID</b></td>
+        <td align="left"><b>Title</b></td>
+        <td align="left"><b>Platform</b></td>
+        <td align="left"><b>Rating</b></td>
+        <td align="left"><b>Released</b></td>
+        <td align="left"><b>Genre</b></td>
+        <td align="left"><b>Company</b></td></tr>
+        
+        
+        <!-- Need database information here -->
+        
+    </table>
+    
+    <!--?php
+        //connect to the database
+        $conn = mysqli_connect("localhost:3306", "root", "", "jet80");
+        //check for failure
+        if ($conn->connect_errno) {
+            printf("connection failed %s\n", $conn->connect_error);
+            exit();
+        }*/
+    ?-->
+    
+    <!--?php
+        //$conn->close();
+    ?-->
 </body>
 </html>
