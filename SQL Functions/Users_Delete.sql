dde5a2103ed984a2b51526c65bb21cc6cb8ea09c
Dragan Jović <dj47@zips.uakron.edu>
2016-11-15 11:18:45 -0500

    Added the Update and Delete functions for Users

---
+++ SQL Functions/Users_Delete.sql
@@ -0,0 +1,7 @@
+DELETE FROM Users
+	WHERE username = 'djovic';
+
+
+Defaut_Code//
+	DELETE FROM table_name
+	WHERE table_being_deleted = value1_being_deleted;
---
+++ SQL Functions/Users_Update.sql
@@ -0,0 +1,10 @@
+UPDATE Users
+	SET accPassword = ‘jack2’
+	WHERE username = ‘jack123’;
+
+
+Default_Code//
+	UPDATE table_name
+	SET column1 = new_value1, column2 = new_value2, …
+	WHERE desired_column = value_being_modified
+]
