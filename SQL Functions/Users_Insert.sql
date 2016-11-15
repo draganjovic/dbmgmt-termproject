3f14e9f715fe8e63ea54fbba07e2da9f738d1c93
Dragan Jović <dj47@zips.uakron.edu>
2016-11-15 11:14:42 -0500

    Insert Users

---
+++ SQL Functions/Users_Insert.sql
@@ -0,0 +1,7 @@
+INSERT into Users (username, accPassword, favoriteTitle, favoritePlatform)
+	values ('djovic', 'jovic123', 'Gears of War 4', 'Xbox One'),
+		('jordan34', 'xboxgamer43', 'Forza Horizon 3', 'Xbox One'),
+		('jack123', 'jack1', 'DOOM', 'Playstation 4'); 
+
+
+
