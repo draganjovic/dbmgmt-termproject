45f8b31e0a8157d06772bb1f592786266970add6
JTEyolf <JTEyolf@users.noreply.github.com>
2016-11-15 18:11:52 -0500

    Removed duplicate entries and changed char lengths to be smaller in some cases

--- Stage 4/Tables/VideoGames.sql
+++ Stage 4/Tables/VideoGames.sql
@@ -1,13 +1,13 @@
 CREATE TABLE VideoGames (
 	
-	title char(200),
-	gameplatform char(200),
-	rating char(200),
+	title char(50),
+	gameplatform char(30),
+	rating char(30),
 	releasedate date,
 	genre char(20),
-	company char(20),
-	summary char(200),
+	company char(30),
+	summary varchar(200),
 
-	PRIMARY KEY(title, gameplatform),
+	PRIMARY KEY(title, gameplatform)
 );
 	
--- Stage 4/Tables/VideoGamesList.sql
+++ Stage 4/Tables/VideoGamesList.sql
@@ -1,60 +1,56 @@
 INSERT into VideoGames (title, gameplatform, rating, releasedate, genre, company, summary)
-	values (‘Far Cry Primal’, ‘Xbox One’, ‘Mature’, ’02/23/2016’, ‘Open World’, ‘Ubisoft’, ‘Taking down mammoths with spears is as satisfying as anything offered in the previous games, and hunting and scavenging for crafting mats is an all-around great experience.’),
+	values ('Far Cry Primal', 'Xbox One', 'Mature', '02/23/2016', 'Open World', 'Ubisoft', 'Taking down mammoths with spears is as satisfying as anything offered in the previous games, and hunting and scavenging for crafting mats is an all-around great experience.'),
 
-		(‘The Division’, ‘Xbox One’, ‘Mature’, ’03/08/2016’, ‘Shooter’, ‘Ubisoft’, ‘It truly has an MMO feel, thanks to the upgrade mechanics, and its skill and ability trees.’),
+		('The Division', 'Xbox One', 'Mature', '03/08/2016', 'Shooter', 'Ubisoft', 'It truly has an MMO feel, thanks to the upgrade mechanics, and its skill and ability trees.'),
 
-		(‘The Witness’, ‘Playstation 4’, ‘Everyone’, ’01/26/2016’, ‘Puzzle’, ‘Thekla’, ‘It’s set in a gorgeous, stylized world, and it’s a puzzle game that will definitely have you scratching your head.’),
+		('The Witness', 'Playstation 4', 'Everyone', '01/26/2016', 'Puzzle', 'Thekla', 'It"s set in a gorgeous, stylized world, and it"s a puzzle game that will definitely have you scratching your head.'),
 
-		(‘Not a Hero’, ‘Playstation 4’, ‘Everyone’, ’02/02/2016’, ‘2D Shooter’, ‘Devolver Digitial’, ‘It follows an anthropomorphic purple rabbit BunnyLord as he travels back in time from the 2048 to become mayor in an effort to save the world.’),
+		('Not a Hero', 'Playstation 4', 'Everyone', '02/02/2016', '2D Shooter', 'Devolver Digitial', 'It follows an anthropomorphic purple rabbit BunnyLord as he travels back in time from the 2048 to become mayor in an effort to save the world.'),
 
-		(‘Gravity Rush Remastered’, ‘Playstation 4’, ‘Everyone’, ’02/02/2016’, ‘Shooter’, ‘Sony’, ‘In the game, players take control of Kat, a strong female character who has gravity-altering abilities that will help her protect her future.’),
+		('Gravity Rush Remastered', 'Playstation 4', 'Everyone', '02/02/2016', 'Shooter', 'Sony', 'In the game, players take control of Kat, a strong female character who has gravity-altering abilities that will help her protect her future.'),
 
-		(‘Dying Light: The Following Expansion’, ‘Playstation 4’, ‘Teen’, ’02/09/2016’, ‘Open World’, ‘WB Entertainment’, ‘Players are able to drive a buggy, smashing through zombies in their path from mission to mission.’),
+		('Dying Light: The Following Expansion', 'Playstation 4', 'Teen', '02/09/2016', 'Open World', 'WB Entertainment', 'Players are able to drive a buggy, smashing through zombies in their path from mission to mission.'),
 
-		(‘Firewatch’, ‘Playstation 4’, ‘Teen’, ’02/09/2016’, ‘Adventure’, ‘Panic Inc’, ‘The game places you in the role of Henry, a man who leaves his life behind to work as a fire lookout in the Wyoming wilderness. Your job is to look for signs of fire, and keep the wilderness safe.’),
+		('Firewatch', 'Playstation 4', 'Teen', '02/09/2016', 'Adventure', 'Panic Inc', 'The game places you in the role of Henry, a man who leaves his life behind to work as a fire lookout in the Wyoming wilderness. Your job is to look for signs of fire, and keep the wilderness safe.'),
 
-		(‘Unravel’, ‘Playstation 4’, ‘Teen’, ’02/09/2016’, ‘Open World’, ‘EA’, ‘You’ll take on the role of Yarny, a cutesy doll created with yarn, and you’re tasked with playing through a hyper realistic world, solving puzzles along the way using unique mechanics only made possible by the fact that you’re made of yarn.’),
+		('Unravel', 'Playstation 4', 'Teen', '02/09/2016', 'Open World', 'EA', 'You"ll take on the role of Yarny, a cutesy doll created with yarn, and you"re tasked with playing through a hyper realistic world, solving puzzles along the way using unique mechanics only made possible by the fact that you"re made of yarn.'),
 	
-		(‘Street Fighter V’, ‘Playstation 4’, ‘Teen’, ’02/16/2016’, ‘Fighting’, ‘Capcom’, ‘It brings new characters into the Street Fighter franchise, each of which has their own unique style of fighting.’),
+		('Street Fighter V', 'Playstation 4', 'Teen', '02/16/2016', 'Fighting', 'Capcom', 'It brings new characters into the Street Fighter franchise, each of which has their own unique style of fighting.'),
 
-		(‘Lego Marvel’s Avengers’, ‘Playstation 4’, ‘Everyone’, ’01/26/2016’, ‘Lego’, ‘WB Interactive’, ‘The game contains a massive roster filled with Marvel superheroes, and some areas in the game require two specific characters to progress any further.’),
+		('Lego Marvel"s Avengers', 'Playstation 4', 'Everyone', '01/26/2016', 'Lego', 'WB Interactive', 'The game contains a massive roster filled with Marvel superheroes, and some areas in the game require two specific characters to progress any further.'),
 
-		(‘Overwatch’, ‘Playstation 4’, ‘Everybody’, ’05/24/2016’, ‘Shooter’, ‘Blizzard’, ‘Overwatch is everything but bland, and it’s the game’s unique flavor that makes it so appealing.’),
+		('Overwatch', 'Playstation 4', 'Everybody', '05/24/2016', 'Shooter', 'Blizzard', 'Overwatch is everything but bland, and it"s the game"s unique flavor that makes it so appealing.'),
 
-		(‘Uncharted 4’, ‘Playstation 4’, ‘Mature’, ’05/10/2016’, ‘Shooter’, ‘Sony’, ‘Uncharted 4 hits all of the right notes, with its incredible visuals and massive set pieces leaving players in awe.’),
+		('Uncharted 4', 'Playstation 4', 'Mature', '05/10/2016', 'Shooter', 'Sony', 'Uncharted 4 hits all of the right notes, with its incredible visuals and massive set pieces leaving players in awe.'),
 
-		(‘Ratchet and Clank’, ‘Playstation 4’, ‘Everyone’, ’04/12/2016’, ‘Adventure’, ‘Sony’, ‘Ratchet and Clank does everything right as it has impressive graphics, incredibly cool guns, and a meta story that is genuinely funny.’),
+		('Ratchet and Clank', 'Playstation 4', 'Everyone', '04/12/2016', 'Adventure', 'Sony', 'Ratchet and Clank does everything right as it has impressive graphics, incredibly cool guns, and a meta story that is genuinely funny.'),
 
-		(‘Madden NFL 17’, ‘Xbox One’, ‘Teen’, ’08/23/2016’, ‘Sports’, ’EA’, ‘We’d fully understand if you’re burnt out on the Madden NFL franchise and sports games in general, as the yearly release schedule can certainly take its toll. But there’s still an undeniable quality set with Madden NFL 17, albeit an incremental notch up from last year’s iteration.’),
+		('Madden NFL 17', 'Xbox One', 'Teen', '08/23/2016', 'Sports', 'EA', 'We"d fully understand if you"re burnt out on the Madden NFL franchise and sports games in general, as the yearly release schedule can certainly take its toll. But there"s still an undeniable quality set with Madden NFL 17, albeit an incremental notch up from last year"s iteration.'),
 
-		(‘Deus Ex: Mankind Divided’, ‘Xbox One’, ‘Teen’, ’08/23/2016’, ‘Open World’, ’Square Enix’, ‘The game brings a major revamp to protagonist Adam Jensen’s augmented abilities, placing him in a cybernetic playground that feels entirely new, although it’s a longstanding franchise that we’ve played, and will continue to play, over and over again.’),
+		('Deus Ex: Mankind Divided', 'Xbox One', 'Teen', '08/23/2016', 'Open World', 'Square Enix', 'The game brings a major revamp to protagonist Adam Jensen"s augmented abilities, placing him in a cybernetic playground that feels entirely new, although it"s a longstanding franchise that we"ve played, and will continue to play, over and over again.'),
 
-		(‘Dark Souls 3’, ‘Playstation 4’, ‘Mature’, ’03/24/2016’, ‘Shooter’, ’Bandai Namco’, ‘The action role-playing game is second to none in its class, and its brutal gameplay will have you begging for mercy.’),
+		('Dark Souls 3', 'Playstation 4', 'Mature', '03/24/2016', 'Shooter', 'Bandai Namco', 'The action role-playing game is second to none in its class, and its brutal gameplay will have you begging for mercy.'),
 
-		(‘DOOM’, ‘PC’, ‘Mature’, ’05/13/2016’, ‘Shooter’, ’Bethesda’, ‘DOOM brings back the frenetic multiplayer that we all loved, with Bethesda giving the game a formidable reboot earlier this year.’),
+		('DOOM', 'PC', 'Mature', '05/13/2016', 'Shooter', 'Bethesda', 'DOOM brings back the frenetic multiplayer that we all loved, with Bethesda giving the game a formidable reboot earlier this year.'),
 
-		(‘King of Fighters XIV’, ‘Playstation 4’, ‘Teen’, ’08/23/2016’, ‘Fighting’, ’Atlus’, ‘The game is filled with deep mechanics that hardcore players will love.’),
+		('King of Fighters XIV', 'Playstation 4', 'Teen', '08/23/2016', 'Fighting', 'Atlus', 'The game is filled with deep mechanics that hardcore players will love.'),
 
-		(‘F1 2016’, ‘Playstation 4’, ‘Teen’, ’08/19/2016’, ‘Racing’, ’Codemasters’, ‘The new career mode is absolutely brilliant, and the game breathes new life into the F1 genre.’),
+		('F1 2016', 'Playstation 4', 'Teen', '08/19/2016', 'Racing', 'Codemasters', 'The new career mode is absolutely brilliant, and the game breathes new life into the F1 genre.'),
 
-		(‘Guilty Gear Xrd -REVELATOR-’, ‘Playstation 4’, ‘Teen’, ’06/07/2016’, ‘Fighting’, ’Aksys Games’, ‘It brings new characters to the game with new mechanics and story, and it has one of the best story modes we’ve seen in a fighting game to date. It also helps that the game runs at a buttery smooth 60 fps, making gameplay look stunning.’),
-
-		(‘King of Fighters XIV’, ‘Playstation 4’, ‘Teen’, ’08/23/2016’, ‘Fighting’, ’Atlus’, ‘The game is filled with deep mechanics that hardcore players will love.’),
+		('Guilty Gear Xrd -REVELATOR-', 'Playstation 4', 'Teen', '06/07/2016', 'Fighting', 'Aksys Games', 'It brings new characters to the game with new mechanics and story, and it has one of the best story modes we"ve seen in a fighting game to date. It also helps that the game runs at a buttery smooth 60 fps, making gameplay look stunning.'),
 		
-		(‘101 Ways To Die’, ‘Xbox One’, ‘Mature’, ’03/18/2016’, ‘Puzzle’, ’Nicalis’, ‘Puzzle game that all gamers will enjoy.’),
+		('101 Ways To Die', 'Xbox One', 'Mature', '03/18/2016', 'Puzzle', 'Nicalis', 'Puzzle game that all gamers will enjoy.'),
 
-		(‘Among the Sleep’, ‘Xbox One’, ‘Mature’, ’06/03/2016’, ‘Adventure, ’Krillbite Studio’, ‘Among the Sleep is a first-person horror adventure game.’),
+		('Among the Sleep', 'Xbox One', 'Mature', '06/03/2016', 'Adventure', 'Krillbite Studio', 'Among the Sleep is a first-person horror adventure game.'),
 
-		(‘Hard Reset Redux’, ‘Xbox One’, ‘Mature’, ’06/03/2016’, ‘Shooter, ’Flying Wild Hog’, ‘The various stages have secret areas with hidden pick ups such as health and ammunition. The environments are designed similarly, as there are explosive barrels and various vending machines outfitted with electro-shock anti-vandalism defenses.’),
+		('Hard Reset Redux', 'Xbox One', 'Mature', '06/03/2016', 'Shooter', 'Flying Wild Hog', 'The various stages have secret areas with hidden pick ups such as health and ammunition. The environments are designed similarly, as there are explosive barrels and various vending machines outfitted with electro-shock anti-vandalism defenses.'),
 
-		(‘Forza Horizon 3’, ‘Xbox One’, ‘Mature’, ’09/27/2016’, ‘Racing, ’Microsoft Studios’, ‘A total of 350 cars are available to players at launch – the most amount of cars to be featured in a Horizon game as of yet.’),
+		('Forza Horizon 3', 'Xbox One', 'Mature', '09/27/2016', 'Racing', 'Microsoft Studios', 'A total of 350 cars are available to players at launch – the most amount of cars to be featured in a Horizon game as of yet.'),
 
-		(‘Fifa 17’, ‘Xbox One’, ‘Teen’, ’09/27/2016’, ‘Sports, ’EA’, ‘The new features in FIFA 17 include new attacking techniques, physical player overhaul, active intelligence system and set piece rewrite.’),
+		('Fifa 17', 'Xbox One', 'Teen', '09/27/2016', 'Sports', 'EA', 'The new features in FIFA 17 include new attacking techniques, physical player overhaul, active intelligence system and set piece rewrite.'),
 
-		(‘Earthlock: Festival of Magic’, ‘Xbox One’, ‘Teen’, ’09/01/2016’, ‘Open World, ’Snowcastle Games’, ‘The games story takes place in the world of Umbra, where a cataclysmic event occurs that stops the planet from spinning.’),
+		('Earthlock: Festival of Magic', 'Xbox One', 'Teen', '09/01/2016', 'Open World', 'Snowcastle Games', 'The games story takes place in the world of Umbra, where a cataclysmic event occurs that stops the planet from spinning.'),
 
-		(‘Gears of War 4’, ‘Xbox One’, ‘Mature’, ’10/11/2016’, ‘Shooter’, ’Microsoft Studios’, ‘Gears of War 4 takes place 25 years after the Imulsion Countermeasure weapon destroyed all Imulsion on the planet Sera, taking the Locust and the Lambent with them as well.’),
+		('Gears of War 4', 'Xbox One', 'Mature', '10/11/2016', 'Shooter', 'Microsoft Studios', 'Gears of War 4 takes place 25 years after the Imulsion Countermeasure weapon destroyed all Imulsion on the planet Sera, taking the Locust and the Lambent with them as well.'),
 
-		(‘Far Cry Primal’, ‘Xbox One’, ‘Mature’, ’02/23/2016’, ‘Shooter’, ’Ubisoft’, ‘The game is set in prehistoric times, the traditional gunplay and vehicle gameplay.’),
-
-		(‘Dead Island’, ‘Xbox One’, ‘Mature’, ’05/31/2016’, ‘Shooter’, ’Techland’, ‘Dead Island features an apparent open world roaming, divided by relatively large areas, and played from a first-person perspective.’);
+('Dead Island', 'Xbox One', 'Mature', '05/31/2016', 'Shooter', 'Techland', 'Dead Island features an apparent open world roaming, divided by relatively large areas, and played from a first-person perspective.');
