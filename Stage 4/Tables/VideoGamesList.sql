ba81a755e74a40ea29b359089189d1f4b6d72490
JTEyolf <JTEyolf@users.noreply.github.com>
2016-11-16 00:28:05 -0500

    Changed the " to \'\'

--- Stage 4/Tables/VideoGamesList.sql
+++ Stage 4/Tables/VideoGamesList.sql
@@ -3,7 +3,7 @@ INSERT into VideoGames (title, gameplatform, rating, releasedate, genre, company
 
 		('The Division', 'Xbox One', 'Mature', '03/08/2016', 'Shooter', 'Ubisoft', 'It truly has an MMO feel, thanks to the upgrade mechanics, and its skill and ability trees.'),
 
-		('The Witness', 'Playstation 4', 'Everyone', '01/26/2016', 'Puzzle', 'Thekla', 'It"s set in a gorgeous, stylized world, and it"s a puzzle game that will definitely have you scratching your head.'),
+		('The Witness', 'Playstation 4', 'Everyone', '01/26/2016', 'Puzzle', 'Thekla', 'It''s set in a gorgeous, stylized world, and it''s a puzzle game that will definitely have you scratching your head.'),
 
 		('Not a Hero', 'Playstation 4', 'Everyone', '02/02/2016', '2D Shooter', 'Devolver Digitial', 'It follows an anthropomorphic purple rabbit BunnyLord as he travels back in time from the 2048 to become mayor in an effort to save the world.'),
 
@@ -13,21 +13,21 @@ INSERT into VideoGames (title, gameplatform, rating, releasedate, genre, company
 
 		('Firewatch', 'Playstation 4', 'Teen', '02/09/2016', 'Adventure', 'Panic Inc', 'The game places you in the role of Henry, a man who leaves his life behind to work as a fire lookout in the Wyoming wilderness. Your job is to look for signs of fire, and keep the wilderness safe.'),
 
-		('Unravel', 'Playstation 4', 'Teen', '02/09/2016', 'Open World', 'EA', 'You"ll take on the role of Yarny, a cutesy doll created with yarn, and you"re tasked with playing through a hyper realistic world, solving puzzles along the way using unique mechanics only made possible by the fact that you"re made of yarn.'),
+		('Unravel', 'Playstation 4', 'Teen', '02/09/2016', 'Open World', 'EA', 'You''ll take on the role of Yarny, a cutesy doll created with yarn, and you''re tasked with playing through a hyper realistic world, solving puzzles along the way using unique mechanics only made possible by the fact that you''re made of yarn.'),
 	
 		('Street Fighter V', 'Playstation 4', 'Teen', '02/16/2016', 'Fighting', 'Capcom', 'It brings new characters into the Street Fighter franchise, each of which has their own unique style of fighting.'),
 
-		('Lego Marvel"s Avengers', 'Playstation 4', 'Everyone', '01/26/2016', 'Lego', 'WB Interactive', 'The game contains a massive roster filled with Marvel superheroes, and some areas in the game require two specific characters to progress any further.'),
+		('Lego Marvel''s Avengers', 'Playstation 4', 'Everyone', '01/26/2016', 'Lego', 'WB Interactive', 'The game contains a massive roster filled with Marvel superheroes, and some areas in the game require two specific characters to progress any further.'),
 
-		('Overwatch', 'Playstation 4', 'Everybody', '05/24/2016', 'Shooter', 'Blizzard', 'Overwatch is everything but bland, and it"s the game"s unique flavor that makes it so appealing.'),
+		('Overwatch', 'Playstation 4', 'Everybody', '05/24/2016', 'Shooter', 'Blizzard', 'Overwatch is everything but bland, and it''s the game''s unique flavor that makes it so appealing.'),
 
 		('Uncharted 4', 'Playstation 4', 'Mature', '05/10/2016', 'Shooter', 'Sony', 'Uncharted 4 hits all of the right notes, with its incredible visuals and massive set pieces leaving players in awe.'),
 
 		('Ratchet and Clank', 'Playstation 4', 'Everyone', '04/12/2016', 'Adventure', 'Sony', 'Ratchet and Clank does everything right as it has impressive graphics, incredibly cool guns, and a meta story that is genuinely funny.'),
 
-		('Madden NFL 17', 'Xbox One', 'Teen', '08/23/2016', 'Sports', 'EA', 'We"d fully understand if you"re burnt out on the Madden NFL franchise and sports games in general, as the yearly release schedule can certainly take its toll. But there"s still an undeniable quality set with Madden NFL 17, albeit an incremental notch up from last year"s iteration.'),
+		('Madden NFL 17', 'Xbox One', 'Teen', '08/23/2016', 'Sports', 'EA', 'We''d fully understand if you''re burnt out on the Madden NFL franchise and sports games in general, as the yearly release schedule can certainly take its toll. But there''s still an undeniable quality set with Madden NFL 17, albeit an incremental notch up from last year''s iteration.'),
 
-		('Deus Ex: Mankind Divided', 'Xbox One', 'Teen', '08/23/2016', 'Open World', 'Square Enix', 'The game brings a major revamp to protagonist Adam Jensen"s augmented abilities, placing him in a cybernetic playground that feels entirely new, although it"s a longstanding franchise that we"ve played, and will continue to play, over and over again.'),
+		('Deus Ex: Mankind Divided', 'Xbox One', 'Teen', '08/23/2016', 'Open World', 'Square Enix', 'The game brings a major revamp to protagonist Adam Jensen''s augmented abilities, placing him in a cybernetic playground that feels entirely new, although it''s a longstanding franchise that we''ve played, and will continue to play, over and over again.'),
 
 		('Dark Souls 3', 'Playstation 4', 'Mature', '03/24/2016', 'Shooter', 'Bandai Namco', 'The action role-playing game is second to none in its class, and its brutal gameplay will have you begging for mercy.'),
 
@@ -37,7 +37,7 @@ INSERT into VideoGames (title, gameplatform, rating, releasedate, genre, company
 
 		('F1 2016', 'Playstation 4', 'Teen', '08/19/2016', 'Racing', 'Codemasters', 'The new career mode is absolutely brilliant, and the game breathes new life into the F1 genre.'),
 
-		('Guilty Gear Xrd -REVELATOR-', 'Playstation 4', 'Teen', '06/07/2016', 'Fighting', 'Aksys Games', 'It brings new characters to the game with new mechanics and story, and it has one of the best story modes we"ve seen in a fighting game to date. It also helps that the game runs at a buttery smooth 60 fps, making gameplay look stunning.'),
+		('Guilty Gear Xrd -REVELATOR-', 'Playstation 4', 'Teen', '06/07/2016', 'Fighting', 'Aksys Games', 'It brings new characters to the game with new mechanics and story, and it has one of the best story modes we''ve seen in a fighting game to date. It also helps that the game runs at a buttery smooth 60 fps, making gameplay look stunning.'),
 		
 		('101 Ways To Die', 'Xbox One', 'Mature', '03/18/2016', 'Puzzle', 'Nicalis', 'Puzzle game that all gamers will enjoy.'),
 
