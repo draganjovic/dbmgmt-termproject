f2b86a9bcda8b081ad1a34c9d34d6e0f5a7e42f6
JTEyolf <JTEyolf@users.noreply.github.com>
2016-11-15 20:51:56 -0500

    Added update functionality

--- accountManagement.php
+++ accountManagement.php
@@ -10,15 +10,15 @@
         <h3>Account Management: </h3>
         
         <div id="accountInformation">
-            <label><b>Username:</b> </label>
             <?php 
+            //make sure site was gotten to correctly
+            if (!empty($_POST['userName'])) {
+                
+                echo '<label><b>Username:</b> </label>';
+            
                 echo $_POST['userName'];
             ?>
-            <br >
-            <label><b>Password:</b> </label>
-            <?php 
-                echo $_POST['password'];
-            ?>
+            <br ><br >
             
             <?php
                 //connect to the database
@@ -29,23 +29,53 @@
                     exit();
                 }
             
-            $qry = "INSERT INTO USERS (username, accPassword, favoriteTitle, favoritePlatform) values ('" . $_POST['userName'] . "', '" . $_POST['password'] . "', NULL, NULL);";
-            
-            if(mysqli_query($conn, $qry)) {
-             echo "Success!";   
+            //new account added (if not new then will look like normal acct page)
+            if (isset($_POST['submit'])) {
+                $qry = "INSERT INTO USERS (username, accPassword, favoriteTitle, favoritePlatform) values ('" . $_POST['userName'] . "', '" . $_POST['password'] . "', NULL, NULL);";
+
+                mysqli_query($conn, $qry);
             }
+            //if favorite updated
             else {
-                echo 'failure';
+                //break the game up into two: one for title and one for gameplatform
+                $gameInfo = explode('|', $_POST['gameChoice']);
+                $qry = "UPDATE USERS SET favoriteTitle = '" . $gameInfo[0] . "', favoritePlatform = '" . $gameInfo[1] . "' WHERE username = '" . $_POST['userName'] . "';";
+                
+                mysqli_query($conn, $qry);
+
             }
+
+            //for updating favorite game
+            echo '<form action="" method="post">';
+            
+            echo '<input type="hidden" name="userName" value="' . $_POST['userName'] . '">';
+            echo 'Favorite: ';
+            $qry = "SELECT title, gameplatform FROM VIDEOGAMES";
+            
+            //get the values from the database and put them in drop down menu
+                if ($result = mysqli_query($conn, $qry)) {
+                    echo '<select name="gameChoice">';
+                    while ($row = mysqli_fetch_array($result)) {
+                        echo '<option value="' . $row['title'] . '|' . $row['gameplatform'] . '">' . $row['title'] . '</option>';
+                    }
+                    echo '</select>  ';
+
+                    $result->close();
+                }
+            
+            echo '<input type="submit" name="updateFav" value="Update Favorite">';
+            
+            echo '</form>';
+            
+            
             
             $conn->close();
+            }
             
+            else {
+                echo "No information to display.";
+            }
             ?>
-        
-        <!--Need to add favorite game option list -->
-        <form action="" method="post">
-            
-        </form>
             
         </div>
     </div>
