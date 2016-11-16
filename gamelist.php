ffd6264713e9d216ba35ffaecf9e69bf0b3614a8
JTEyolf <JTEyolf@users.noreply.github.com>
2016-11-15 23:56:04 -0500

    Added search function and signup link

--- gamelist.php
+++ gamelist.php
@@ -5,32 +5,45 @@
     <link rel="stylesheet" type="text/css" href="styles.css">
 </head>
 <body>
-    <h1>Videogames of 2016</h1><hr >
-    
-    <table align="left" cellspacing="5" cellpadding="8">
-        <tr>
-            <td align="left"><b>Title</b></td>
-            <td align="left"><b>Platform</b></td>
-            <td align="left"><b>Released</b></td>
-            <td align="left"><b>Rating</b></td>
-            <td align="left"><b>Genre</b></td>
-            <td align="left"><b>Company</b></td>
-        </tr>
-        
-        
-        <!-- Need database information here with game search-->
-    
-    <?php
-        //connect to the database
-        $conn = mysqli_connect("localhost:3306", "root", "", "dbmgmt");
-        //check for failure
-        if ($conn->connect_errno) {
-            printf("connection failed %s\n", $conn->connect_error);
-            exit();
-        }
-        $qry = "SELECT * FROM VIDEOGAMES";
-    
-        //get the values from the database and print them out
+    <div id="gamelistOverall">
+        <h1>Videogames of 2016</h1>
+        <div id="signup">
+            <a href="accountcreate.php">Sign Up</a>
+        </div>
+        <hr >
+
+        <table align="left" cellspacing="5" cellpadding="8">
+            <tr>
+                <td align="left"><b>Title</b></td>
+                <td align="left"><b>Platform</b></td>
+                <td align="left"><b>Released</b></td>
+                <td align="left"><b>Rating</b></td>
+                <td align="left"><b>Genre</b></td>
+                <td align="left"><b>Company</b></td>
+            </tr>
+
+        <?php
+            //connect to the database
+            $conn = mysqli_connect("localhost:3306", "root", "", "dbmgmt");
+            //check for failure
+            if ($conn->connect_errno) {
+                printf("connection failed %s\n", $conn->connect_error);
+                exit();
+            }
+            $qry = "SELECT * FROM VIDEOGAMES";
+
+            //search for a value
+            echo '<form action="" method="post">';
+            echo '<br /><div id="search"><b>Search By: </b><input type="text" name="searchString" size="30" maxlength="50">  <select name="searchtype">';
+            echo '<option value="title">Title</option><option value="gameplatform">Platform</option><option value="rating">Rating</option><option value="genre">Genre</option><option value="company">Company</option>';
+            echo '</select>';
+            echo '  <input type="submit" name="search" value="Search"></div></form>';
+
+            if(isset($_POST['search'])) {
+                $qry = "SELECT * FROM VIDEOGAMES WHERE " . $_POST['searchtype'] . " LIKE '" . $_POST['searchString'] . "%'";
+            }
+
+            //get the values from the database and print them out
                 if ($result = mysqli_query($conn, $qry)) {
                     while ($row = mysqli_fetch_array($result)) {
                         echo '<tr><td align="left">' . 
@@ -43,16 +56,18 @@
                             '</tr>';
                     }
 
-                echo '</table>';
+                    echo '</table>';
 
                     $result->close();
                 }
-        
-    ?>
-        
-    
-    <?php
-        $conn->close();
-    ?>
+
+        ?>
+
+
+        <?php
+            $conn->close();
+        ?>
+
+    </div>
 </body>
 </html>
