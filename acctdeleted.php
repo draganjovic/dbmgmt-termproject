0f7abf27adf174d592ac981b4f136dfa0e796d77
JTEyolf <JTEyolf@users.noreply.github.com>
2016-11-15 23:19:12 -0500

    For deleting an account

---
+++ acctdeleted.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Account Deleted</title>
+</head>
+    
+<body>
+
+    <?php
+        
+        header('Refresh: 5;url=gamelist.php');
+        if (isset($_POST['deleteacct'])) {
+            //connect to the database
+            $conn = mysqli_connect("localhost:3306", "root", "", "dbmgmt");
+            //check for failure
+            if ($conn->connect_errno) {
+                printf("connection failed %s\n", $conn->connect_error);
+                exit();
+            }
+
+            $qry = "DELETE FROM USERS WHERE username = '" . $_POST['userName'] . "';";
+            if (mysqli_query($conn, $qry)) {
+                echo "Account Sucessfully Deleted";
+            }
+
+            $conn->close();
+        }
+        else
+            echo "Nothing to delete.";
+    ?>
+    
+</body>
+
+</html>
