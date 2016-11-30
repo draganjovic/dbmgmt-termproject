384e388c93507e259a8faaadf5ba75bd4c857464
Dragan Jović <dj47@zips.uakron.edu>
2016-11-30 14:43:51 -0500

    Added "images" data type to the table.

---
+++ VideoGames.sql
@@ -0,0 +1,14 @@
+CREATE TABLE VideoGames (
+	
+	title char(200),
+	gameplatform char(200),
+	rating char(200),
+	releasedate date,
+    image blob,
+	genre char(20),
+	company char(20),
+	summary char(200),
+
+	PRIMARY KEY(title, gameplatform),
+);
+	
