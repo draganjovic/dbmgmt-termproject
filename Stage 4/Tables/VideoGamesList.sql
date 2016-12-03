168f31d50c5ac663e5e9f2476134565efac4b99d
Dragan Jović <dj47@zips.uakron.edu>
2016-12-03 18:02:40 -0500

    Added the rest of the images

--- Stage 4/Tables/VideoGamesList.sql
+++ Stage 4/Tables/VideoGamesList.sql
@@ -21,40 +21,40 @@ INSERT into VideoGames (title, gameplatform, rating, releasedate, image, genre, 
 
 		(‘Overwatch’, ‘Playstation 4’, ‘Everybody’, ’05/24/2016’, ’https://www.furiouspaul.com/overwatch/images/ow-symbol.gif’, ‘Shooter’, ‘Blizzard’, ‘Overwatch is everything but bland, and it’s the game’s unique flavor that makes it so appealing.’),
 
-		(‘Uncharted 4’, ‘Playstation 4’, ‘Mature’, ’05/10/2016’, ‘Shooter’, ‘Sony’, ‘Uncharted 4 hits all of the right notes, with its incredible visuals and massive set pieces leaving players in awe.’),
+		(‘Uncharted 4’, ‘Playstation 4’, ‘Mature’, ’05/10/2016’, 'http://i1.wp.com/www.brashgames.co.uk/wp-content/uploads/2014/12/Uncharted-4-A-Thiefs-End-PS4-Cover.jpg?resize=200%2C200', ‘Shooter’, ‘Sony’, ‘Uncharted 4 hits all of the right notes, with its incredible visuals and massive set pieces leaving players in awe.’),
 
-		(‘Ratchet and Clank’, ‘Playstation 4’, ‘Everyone’, ’04/12/2016’, ‘Adventure’, ‘Sony’, ‘Ratchet and Clank does everything right as it has impressive graphics, incredibly cool guns, and a meta story that is genuinely funny.’),
+		(‘Ratchet and Clank’, ‘Playstation 4’, ‘Everyone’, ’04/12/2016’, 'http://is3.mzstatic.com/image/thumb/Music49/v4/0e/76/ef/0e76ef8d-4471-8a84-598e-3d5ab1d1485f/source/200x200bb.jpg', ‘Adventure’, ‘Sony’, ‘Ratchet and Clank does everything right as it has impressive graphics, incredibly cool guns, and a meta story that is genuinely funny.’),
 
-		(‘Madden NFL 17’, ‘Xbox One’, ‘Teen’, ’08/23/2016’, ‘Sports’, ’EA’, ‘We’d fully understand if you’re burnt out on the Madden NFL franchise and sports games in general, as the yearly release schedule can certainly take its toll. But there’s still an undeniable quality set with Madden NFL 17, albeit an incremental notch up from last year’s iteration.’),
+		(‘Madden NFL 17’, ‘Xbox One’, ‘Teen’, ’08/23/2016’, 'http://www.brashgames.co.uk/wp-content/uploads/2016/09/Madden-NFL-17-Review.jpg', ‘Sports’, ’EA’, ‘We’d fully understand if you’re burnt out on the Madden NFL franchise and sports games in general, as the yearly release schedule can certainly take its toll. But there’s still an undeniable quality set with Madden NFL 17, albeit an incremental notch up from last year’s iteration.’),
 
-		(‘Deus Ex: Mankind Divided’, ‘Xbox One’, ‘Teen’, ’08/23/2016’, ‘Open World’, ’Square Enix’, ‘The game brings a major revamp to protagonist Adam Jensen’s augmented abilities, placing him in a cybernetic playground that feels entirely new, although it’s a longstanding franchise that we’ve played, and will continue to play, over and over again.’),
+		(‘Deus Ex: Mankind Divided’, ‘Xbox One’, ‘Teen’, ’08/23/2016’, 'http://www.makinggames.biz/wp-content/uploads/2016/08/deus-ex-mankind-divided-teaser_11572.jpg', ‘Open World’, ’Square Enix’, ‘The game brings a major revamp to protagonist Adam Jensen’s augmented abilities, placing him in a cybernetic playground that feels entirely new, although it’s a longstanding franchise that we’ve played, and will continue to play, over and over again.’),
 
-		(‘Dark Souls 3’, ‘Playstation 4’, ‘Mature’, ’03/24/2016’, ‘Shooter’, ’Bandai Namco’, ‘The action role-playing game is second to none in its class, and its brutal gameplay will have you begging for mercy.’),
+		(‘Dark Souls 3’, ‘Playstation 4’, ‘Mature’, ’03/24/2016’, 'http://compass.xbox.com/assets/ae/68/ae68e2d6-9256-41c5-9540-ab2962433551.jpg?n=dark-souls-3_facebook_200x200.jpg', ‘Shooter’, ’Bandai Namco’, ‘The action role-playing game is second to none in its class, and its brutal gameplay will have you begging for mercy.’),
 
-		(‘DOOM’, ‘PC’, ‘Mature’, ’05/13/2016’, ‘Shooter’, ’Bethesda’, ‘DOOM brings back the frenetic multiplayer that we all loved, with Bethesda giving the game a formidable reboot earlier this year.’),
+		(‘DOOM’, ‘PC’, ‘Mature’, ’05/13/2016’, 'https://community.bethesda.net/community/image/2306/1.png?a=6433', ‘Shooter’, ’Bethesda’, ‘DOOM brings back the frenetic multiplayer that we all loved, with Bethesda giving the game a formidable reboot earlier this year.’),
 
-		(‘King of Fighters XIV’, ‘Playstation 4’, ‘Teen’, ’08/23/2016’, ‘Fighting’, ’Atlus’, ‘The game is filled with deep mechanics that hardcore players will love.’),
+		(‘King of Fighters XIV’, ‘Playstation 4’, ‘Teen’, ’08/23/2016’, 'http://www.technobuffalo.com/wp-content/uploads/2015/09/King-of-Fighters-XIV-2-200x200.jpg', ‘Fighting’, ’Atlus’, ‘The game is filled with deep mechanics that hardcore players will love.’),
 
-		(‘F1 2016’, ‘Playstation 4’, ‘Teen’, ’08/19/2016’, ‘Racing’, ’Codemasters’, ‘The new career mode is absolutely brilliant, and the game breathes new life into the F1 genre.’),
+		(‘F1 2016’, ‘Playstation 4’, ‘Teen’, ’08/19/2016’, 'https://www.gtplanet.net/wp-content/uploads/2016/08/F1-2016-Banner-Image-e1471626052879-200x200.jpg', ‘Racing’, ’Codemasters’, ‘The new career mode is absolutely brilliant, and the game breathes new life into the F1 genre.’),
 
-		(‘Guilty Gear Xrd -REVELATOR-’, ‘Playstation 4’, ‘Teen’, ’06/07/2016’, ‘Fighting’, ’Aksys Games’, ‘It brings new characters to the game with new mechanics and story, and it has one of the best story modes we’ve seen in a fighting game to date. It also helps that the game runs at a buttery smooth 60 fps, making gameplay look stunning.’),
+		(‘Guilty Gear Xrd -REVELATOR-’, ‘Playstation 4’, ‘Teen’, ’06/07/2016’,'http://www.gry-online.pl/galeria/gry13/10920569.jpg', ‘Fighting’, ’Aksys Games’, ‘It brings new characters to the game with new mechanics and story, and it has one of the best story modes we’ve seen in a fighting game to date. It also helps that the game runs at a buttery smooth 60 fps, making gameplay look stunning.’),
 
-		(‘King of Fighters XIV’, ‘Playstation 4’, ‘Teen’, ’08/23/2016’, ‘Fighting’, ’Atlus’, ‘The game is filled with deep mechanics that hardcore players will love.’),
+		(‘King of Fighters XI’, ‘Playstation 4’, ‘Teen’, ’08/23/2016’, 'http://img.kbhgames.com/2016/02/The-King-of-Fighters-2002.jpg', ‘Fighting’, ’Atlus’, ‘The game is filled with deep mechanics that hardcore players will love.’),
 		
-		(‘101 Ways To Die’, ‘Xbox One’, ‘Mature’, ’03/18/2016’, ‘Puzzle’, ’Nicalis’, ‘Puzzle game that all gamers will enjoy.’),
+		(‘101 Ways To Die’, ‘Xbox One’, ‘Mature’, ’03/18/2016’, 'http://xboxtavern.com/wp-content/uploads/2016/03/101waystodierevlogo.jpg', ‘Puzzle’, ’Nicalis’, ‘Puzzle game that all gamers will enjoy.’),
 
-		(‘Among the Sleep’, ‘Xbox One’, ‘Mature’, ’06/03/2016’, ‘Adventure, ’Krillbite Studio’, ‘Among the Sleep is a first-person horror adventure game.’),
+		(‘Among the Sleep’, ‘Xbox One’, ‘Mature’, ’06/03/2016’, 'http://www.technobuffalo.com/wp-content/uploads/2014/03/among-the-sleep-trailer-200x200.jpg', ‘Adventure', ’Krillbite Studio’, ‘Among the Sleep is a first-person horror adventure game.’),
 
-		(‘Hard Reset Redux’, ‘Xbox One’, ‘Mature’, ’06/03/2016’, ‘Shooter, ’Flying Wild Hog’, ‘The various stages have secret areas with hidden pick ups such as health and ammunition. The environments are designed similarly, as there are explosive barrels and various vending machines outfitted with electro-shock anti-vandalism defenses.’),
+		(‘Hard Reset Redux’, ‘Xbox One’, ‘Mature’, ’06/03/2016’, 'http://www.brashgames.co.uk/wp-content/uploads/2016/06/Hard-Reset-Redux-Review.jpg', ‘Shooter', ’Flying Wild Hog’, ‘The various stages have secret areas with hidden pick ups such as health and ammunition. The environments are designed similarly, as there are explosive barrels and various vending machines outfitted with electro-shock anti-vandalism defenses.’),
 
-		(‘Forza Horizon 3’, ‘Xbox One’, ‘Mature’, ’09/27/2016’, ‘Racing, ’Microsoft Studios’, ‘A total of 350 cars are available to players at launch – the most amount of cars to be featured in a Horizon game as of yet.’),
+		(‘Forza Horizon 3’, ‘Xbox One’, ‘Mature’, ’09/27/2016’, 'https://www.gtplanet.net/wp-content/uploads/2016/06/Forza-Horizon-3-04-e1467472809258-200x200.jpg', ‘Racing', ’Microsoft Studios’, ‘A total of 350 cars are available to players at launch – the most amount of cars to be featured in a Horizon game as of yet.’),
 
-		(‘Fifa 17’, ‘Xbox One’, ‘Teen’, ’09/27/2016’, ‘Sports, ’EA’, ‘The new features in FIFA 17 include new attacking techniques, physical player overhaul, active intelligence system and set piece rewrite.’),
+		(‘Fifa 17’, ‘Xbox One’, ‘Teen’, ’09/27/2016’, 'http://www.dlcompare.com/blog/wp-content/uploads/2016/09/fifa.jpg', ‘Sports', ’EA’, ‘The new features in FIFA 17 include new attacking techniques, physical player overhaul, active intelligence system and set piece rewrite.’),
 
-		(‘Earthlock: Festival of Magic’, ‘Xbox One’, ‘Teen’, ’09/01/2016’, ‘Open World, ’Snowcastle Games’, ‘The games story takes place in the world of Umbra, where a cataclysmic event occurs that stops the planet from spinning.’),
+		(‘Earthlock: Festival of Magic’, ‘Xbox One’, ‘Teen’, ’09/01/2016’, 'http://xboxtavern.com/wp-content/uploads/2016/09/earthlocrevlogo55.jpg', ‘Open World', ’Snowcastle Games’, ‘The games story takes place in the world of Umbra, where a cataclysmic event occurs that stops the planet from spinning.’),
 
-		(‘Gears of War 4’, ‘Xbox One’, ‘Mature’, ’10/11/2016’, ‘Shooter’, ’Microsoft Studios’, ‘Gears of War 4 takes place 25 years after the Imulsion Countermeasure weapon destroyed all Imulsion on the planet Sera, taking the Locust and the Lambent with them as well.’),
+		(‘Gears of War 4’, ‘Xbox One’, ‘Mature’, ’10/11/2016’, 'http://xboxtavern.com/wp-content/uploads/2016/10/gearsofwar4revlogo55.jpg', ‘Shooter’, ’Microsoft Studios’, ‘Gears of War 4 takes place 25 years after the Imulsion Countermeasure weapon destroyed all Imulsion on the planet Sera, taking the Locust and the Lambent with them as well.’),
 
-		(‘Far Cry Primal’, ‘Xbox One’, ‘Mature’, ’02/23/2016’, ‘Shooter’, ’Ubisoft’, ‘The game is set in prehistoric times, the traditional gunplay and vehicle gameplay.’),
+		(‘Far Cry Primal’, ‘Xbox One’, ‘Mature’, ’02/23/2016’, 'https://pp.vk.me/c633925/v633925943/11ebe/9KBIweWogRw.jpg', ‘Shooter’, ’Ubisoft’, ‘The game is set in prehistoric times, the traditional gunplay and vehicle gameplay.’),
 
-		(‘Dead Island’, ‘Xbox One’, ‘Mature’, ’05/31/2016’, ‘Shooter’, ’Techland’, ‘Dead Island features an apparent open world roaming, divided by relatively large areas, and played from a first-person perspective.’);
+		(‘Dead Island’, ‘Xbox One’, ‘Mature’, ’05/31/2016’, 'http://skipgameintro.altervista.org/wp-content/uploads/2014/09/dead-island-200x200.jpg', ‘Shooter’, ’Techland’, ‘Dead Island features an apparent open world roaming, divided by relatively large areas, and played from a first-person perspective.’);
